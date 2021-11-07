ab40aa2ef39eb20b5b541f483072e3e70d4b8231
Nick Smirnov <smirnovnickolay00@gmail.com>
2021-11-07 18:44:48 +0300

    весь проект

---
+++ spec/articles_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'rails_helper'
+require_relative 'C:/Users/Nick Smirnov/RoR/blog/app/controllers/articles_controller.rb'
+describe ArticlesController < ApplicationController do
+	describe '.create' do
+		it 'sets the name' do
+			articles = Article.new(title: 'Ruby_test', body: 'testing')
+			
+			expect(articles.title).to eq 'Ruby_test'
+		end
+  end
+
+end
+
